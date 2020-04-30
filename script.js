4f6c55a2288e1491cb2d71c021980e3c360e6f9b
MornsCanCode <marvellousn561@gmail.com>
2020-04-30 12:40:29 +0000

    Renamed

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-var randomNumber = Math.random() * 7;
-console.log(randomNumber)
+var randomNumber = Math.floor( Math.random() * 7);
+var UserInput = prompt ('Think of a number between 1 and 10');
+var userInputToFloat = parseFloat(UserInput);
+const writeInDiv = document.getElementById('app');
+
+if (userInputToFloat ===randomNumber){
+ writeInDiv.innerHTML = 'Right as rain!'
+}else{
+  writeInDiv.innerHTML = `OMG! I can't believe you did it. <br><br><br><br> Just being sarcastic, you were wrong. Guess again` 
+}
