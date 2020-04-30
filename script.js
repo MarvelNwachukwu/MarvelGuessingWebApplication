720ff9797aa1268bb0daaf9c130fe6f326f1fb04
Marvellous Nwachukwu <marvellousn561@gmail.com>
2020-04-30 19:34:56 +0000

    Improved functionality

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-var randomNumber = Math.floor( Math.random() * 7);
-var UserInput = prompt ('Think of a number between 1 and 10');
+var randomNumber = Math.floor( Math.random() * 1000);
+var Name = prompt('What is your name?');
+var UserInput = prompt ('Think of a number between 1 and 1000');
 var userInputToFloat = parseFloat(UserInput);
 const writeInDiv = document.getElementById('app');
 
-if (userInputToFloat ===randomNumber){
- writeInDiv.innerHTML = 'Right as rain!'
+
+console.log(randomNumber);
+
+
+if (userInputToFloat === randomNumber){
+ writeInDiv.innerHTML = `Brilliant minds will always win. I'm so proud of you, buddy. <br><br> DM me with a screenshot of this page, I owe you. `
 }else{
-  writeInDiv.innerHTML = `OMG! I can't believe you did it. <br><br><br><br> Just being sarcastic, you were wrong. Guess again` 
-}
+  writeInDiv.innerHTML = `Come on ${Name}, you can guess something better than ${UserInput}. By the way, the number was ${randomNumber}.<br><br><br> Why don't you try again.`
+};
